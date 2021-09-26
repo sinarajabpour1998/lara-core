171ab00c358a1718b6db2ae62b3be5000bc0b6a9
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-09-27 00:35:14 +0330

    Replaced permission names

--- src/views/components/acl_menu.blade.php
+++ src/views/components/acl_menu.blade.php
@@ -1,4 +1,4 @@
-@if(Auth()->user()->isAbleTo([Config::get('acl-manager.permissions.menu.main')]))
+@if(Auth()->user()->isAbleTo([Config::get('lara-core.permissions.menu.main')]))
     <li class="treeview {{ isActive( ['roles-assignment.index', 'roles.index', 'permissions.index'], 'is-expanded' ) }}">
         <a class="app-menu__item" href="#" data-toggle="treeview">
             <i class="app-menu__icon fa fa-users"></i>
@@ -6,21 +6,21 @@
             <i class="treeview-indicator fa fa-angle-left"></i>
         </a>
         <ul class="treeview-menu">
-            @if(Auth()->user()->isAbleTo([Config::get('acl-manager.permissions.menu.users')]))
+            @if(Auth()->user()->isAbleTo([Config::get('lara-core.permissions.menu.users')]))
                 <li>
                     <a class="treeview-item pl-3 {{ isActive('roles-assignment.index') }}" href="{{ route('roles-assignment.index') }}">
                         <i class="icon fa fa-circle-o"></i>کاربران
                     </a>
                 </li>
             @endif
-            @if(Auth()->user()->isAbleTo([Config::get('acl-manager.permissions.menu.roles')]))
+            @if(Auth()->user()->isAbleTo([Config::get('lara-core.permissions.menu.roles')]))
                 <li>
                     <a class="treeview-item pl-3 {{ isActive('roles.index') }}" href="{{ route('roles.index') }}">
                         <i class="icon fa fa-circle-o"></i>نقش‌ها
                     </a>
                 </li>
             @endif
-            @if(Auth()->user()->isAbleTo([Config::get('acl-manager.permissions.menu.permissions')]))
+            @if(Auth()->user()->isAbleTo([Config::get('lara-core.permissions.menu.permissions')]))
                 <li>
                     <a class="treeview-item pl-3 {{ isActive('permissions.index') }}" href="{{ route('permissions.index') }}">
                         <i class="icon fa fa-circle-o"></i>‌دسترسی‌ها
